5436f3d3839a795118b925ae2892e206083a892c
Håkan Fahlstedt <hf@happycoders.agency>
2021-05-11 21:09:21 +0200

    Ny temp bild

--- src/components/BoardMember.tsx
+++ src/components/BoardMember.tsx
@@ -9,10 +9,7 @@ export default function BoardMember({ name, title }: BoardMemberProps) {
       <div className="card">
         <div className="card-image">
           <figure className="image is-4by3">
-            <img
-              src="https://bulma.io/images/placeholders/1280x960.png"
-              alt="Placeholder image"
-            />
+            <img src="/images/img.png" alt="Placeholder image" />
           </figure>
         </div>
         <div className="card-content">

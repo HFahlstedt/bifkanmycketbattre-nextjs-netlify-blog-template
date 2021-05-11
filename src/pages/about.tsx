498f2f0ccc9b5b78f7d3f6e4e42c5db398f54b1f
Håkan Fahlstedt <hf@happycoders.agency>
2021-05-11 21:41:17 +0200

    about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,10 +13,51 @@ export default function About() {
       <div className="container">
         <article>
           <header>
-            <h1 className="title is-2">Vilka är vi</h1>
+            <h1 className="title is-2 mb-5">Vilka är vi</h1>
           </header>
           <div className="content">
-            <p></p>
+            <p>
+              Jonas Sundqvist,{" "}
+              <span className="has-text-grey">
+                medlem 6857 (fd ledare i Brynäs Juniorer, Hockeygymnasiet och fd
+                medlem i valberedningen)
+              </span>
+            </p>
+            <p>
+              Micke Pettersson,{" "}
+              <span className="has-text-grey">
+                ständig medlem 10004 (grundare och fd ordförande Brynäs
+                Stockholm, fd medlem av medlemsrådet och hemsidesgruppen Brynäs
+                IF)
+              </span>
+            </p>
+            <p>
+              Henrik Nordström,
+              <span className="has-text-grey">medlem 6752</span>
+            </p>
+            <p>
+              Jon Pierre, <span className="has-text-grey">ständig medlem</span>
+            </p>
+            <p>
+              Dan Ekholm,
+              <span className="has-text-grey">
+                medlem 11164 (fd medlem i valberedningen och mångårigt engagerad
+                inom Brynäs IF)
+              </span>
+            </p>
+            <p>
+              Håkan Fahlstedt,
+              <span className="has-text-grey">
+                ständig medlem 7926 (fd ordförande Brooklyn Tigers)
+              </span>
+            </p>
+            <p>
+              Torbjörn Risve,
+              <span className="has-text-grey">medlem 15323</span>
+            </p>
+            <p>
+              Stefan Olsson, <span className="has-text-grey">medlem 41103</span>
+            </p>
           </div>
         </article>
       </div>
